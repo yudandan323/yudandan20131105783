89d5453b13ef5dbe5ed2a84c246e839e878da497
yudandan323 <865219137@qq.com>
2016-01-01 20:01:01 +0800

    已经做好获取摄像头，正在向程序里添加人脸识别

--- open2.4.0摄像头的获取/open2.4.0人脸/open2.4.0人脸.cpp
+++ open2.4.0摄像头的获取/open2.4.0人脸/open2.4.0人脸.cpp
@@ -1,10 +1,34 @@
 // open2.4.0人脸.cpp : 定义控制台应用程序的入口点。
 //
+
+#include "string.h"
 #include "stdafx.h"  
 #include <cv.h>  
 #include <cxcore.h>  
 #include <highgui.h> 
 #include<stdio.h>
+#include<opencv2/imgproc//imgproc.hpp>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/objdetect/objdetect.hpp>
+using namespace cv;
+using namespace std;
+string face_cascade_name="E:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt2.xml";
+CascadeClassifier face_cascade; 
+string window_name="人脸识别";
+void detectAndDisplay(Mat frame){
+	std::vector<Rect> faces;
+	Mat frame_gray;  
+	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	equalizeHist( frame_gray, frame_gray ); 
+	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30));
+	 for( int i = 0; i < faces.size(); i++ ){  
+		 Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );  
+		 ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+	 }
+	 imshow(window_name,frame);
+
+}
 //实现将拍下来的图片缩放一倍
 IplImage* DoPyrDown(IplImage* image,int filter=IPL_GAUSSIAN_5x5)
 {
