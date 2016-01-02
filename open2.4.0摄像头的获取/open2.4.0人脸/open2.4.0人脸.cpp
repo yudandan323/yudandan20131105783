36782a6b6298d625bc9d235839612645ba02f1ba
yudandan323 <865219137@qq.com>
2016-01-02 09:21:17 +0800

    摄像头获取，实现检测人脸，人眼，人的鼻子

--- open2.4.0摄像头的获取/open2.4.0人脸/open2.4.0人脸.cpp
+++ open2.4.0摄像头的获取/open2.4.0人脸/open2.4.0人脸.cpp
@@ -7,14 +7,19 @@
 #include <cxcore.h>  
 #include <highgui.h> 
 #include<stdio.h>
-#include<opencv2/imgproc//imgproc.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/objdetect/objdetect.hpp>
+#include<opencv2/video/video.hpp>
 using namespace cv;
 using namespace std;
 string face_cascade_name="E:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt2.xml";
+String eyes_cascade_name = "E:/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+String nose_cascade_name = "E:/opencv/sources/data/haarcascades/haarcascade_mcs_nose.xml";
 CascadeClassifier face_cascade; 
+CascadeClassifier eyes_cascade;
+CascadeClassifier nose_cascade;
 string window_name="人脸识别";
 void detectAndDisplay(Mat frame){
 	std::vector<Rect> faces;
@@ -22,10 +27,30 @@ void detectAndDisplay(Mat frame){
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray ); 
 	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30));
-	 for( int i = 0; i < faces.size(); i++ ){  
+	 for(int  i = 0; i < faces.size(); i++ ){  
 		 Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );  
-		 ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-	 }
+		 ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );//用椭圆框出脸
+	
+	 Mat faceROI = frame_gray(faces[i]);
+	 std::vector<Rect> eyes; 
+	 eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30)); 
+	  for (int j = 0; j < eyes.size(); j++)
+	  {
+		  Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2); 
+		  int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);  
+		  circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);//-- 蓝色圆框出眼睛
+
+	  }
+	  std::vector<Rect> nose; 
+	  nose_cascade.detectMultiScale( faceROI, nose, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+	  for ( int j = 0; j< nose.size(); j++ )
+	  {
+		   Point nose_center( faces[i].x + nose[j].x + nose[j].width/2, faces[i].y + nose[j].y + nose[j].height/2 ); 
+		   int radius = cvRound( (nose[j].width + nose[j].height)*0.25 );
+		   circle( frame, nose_center, radius, Scalar( 0, 255, 0 ), 4, 8, 0 );//-- 蓝色圆框出眼睛  
+
+	  }
+	   }
 	 imshow(window_name,frame);
 
 }
@@ -45,7 +70,15 @@ int main(int argc, char** argv )
 		return -1;
 	Mat edges;
 	if(!face_cascade.load(face_cascade_name)){
-		printf("[error]无法加载级联分类器文件！\n");
+		printf("[error]loading face cascade！\n");
+		return -1;
+	}
+	if(!eyes_cascade.load(eyes_cascade_name)){
+		printf("[error]loading face cascade！\n");
+		return -1;
+	}
+	if(!nose_cascade.load(nose_cascade_name)){
+		printf("[error]loading nose cascade！\n");
 		return -1;
 	}
 	int nTick=0;
